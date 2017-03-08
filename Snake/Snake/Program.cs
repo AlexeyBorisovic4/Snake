cc12274ab4b52e1cdf78e056bf41f5fa0ba33ad3
Алексей <alex9-smirnov@yandex.ru>
2017-03-08 17:27:37 +0500

    Список

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,18 +10,41 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            //p1.x = 1;
-            //p1.y = 3;
-            //p1.sym = '*';
+            Point p1 = new Point(1, 3, '*');           
             p1.Draw();
             
 
-            Point p2 = new Point(4, 5, '#');
-            //p2.x = 4;
-            //p2.y = 5;
-            //p2.sym = '#';
-            p2.Draw();           
+            Point p2 = new Point(4, 5, '#');           
+            p2.Draw();
+
+            Point p3 = new Point(10, 10, '&');
+            p3.Draw();
+
+            Point p4 = new Point(11, 11, '!');
+            p4.Draw();
+
+            List<char> symList = new List<char>();
+            symList.Add('!');
+            symList.Add('*');
+            symList.Add('&');
+            symList.Add('%');
+            
+            foreach (var i in symList)
+            {
+                //Console.WriteLine(i);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach (var p in symList)
+            {
+                //Console.WriteLine(p);
+            }
+
             Console.ReadKey();
         }      
     }
