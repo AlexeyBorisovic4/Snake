626502391b1e4ec9fbe84be8a6c77a275a34c286
Алексей <alex9-smirnov@yandex.ru>
2017-03-10 11:45:07 +0500

    class HorizontalLine

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -17,33 +17,10 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');           
             p2.Draw();
 
-            Point p3 = new Point(10, 10, '&');
-            p3.Draw();
-
-            Point p4 = new Point(11, 11, '!');
-            p4.Draw();
-
-            List<char> symList = new List<char>();
-            symList.Add('!');
-            symList.Add('*');
-            symList.Add('&');
-            symList.Add('%');
-            
-            foreach (var i in symList)
-            {
-                //Console.WriteLine(i);
-            }
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-
-            foreach (var p in symList)
-            {
-                //Console.WriteLine(p);
-            }
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
+        
+          
 
             Console.ReadKey();
         }      
