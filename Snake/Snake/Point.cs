51886e2d4c499b33e1bf69b25f9c6194773660be
Алексей <alex9-smirnov@yandex.ru>
2017-03-10 13:09:22 +0500

    Class snake

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -24,6 +24,35 @@ namespace Snake
             sym = _sym;
         }
 
+        public void Move(int offset, Direction direction)
+        {
+            if (direction ==Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT)
+                    {
+                x = x - offset;
+            }
+
+            else if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+
+            else if (direction == Direction.DOWN)
+            {
+                y = y - offset;
+            }
+        }
+
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -26,7 +26,8 @@ namespace Snake
 
             //орисовка точки
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadKey();
         }      
